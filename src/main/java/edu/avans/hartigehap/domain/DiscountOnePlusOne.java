1f110ac8122bd9718804eacce9f7f23a9e8f25a7
maart <majbroek1@student.avans.nl>
2016-04-12 15:34:19 +0200

    Strategy Singleton added Strategy fixed

--- src/main/java/edu/avans/hartigehap/domain/DiscountOnePlusOne.java
+++ src/main/java/edu/avans/hartigehap/domain/DiscountOnePlusOne.java
@@ -1,36 +1,58 @@
 package edu.avans.hartigehap.domain;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
 /**
  * Created by Student on 21-03-16.
  */
 public class DiscountOnePlusOne extends DiscountStrategy {
-    public int CalculateDiscount(Bill bill){
-        int discountPrice = 0;
-        ArrayList<MenuItem> menuItems = this.getMenuItemList(bill);
+    
+	public int CalculateDiscount(Bill bill){
+		ArrayList<OrderItem> orderItems = new ArrayList();
+		for (Order order : bill.getAllOrders()){
+			orderItems.addAll(order.getOrderItems());
+		}
+		ArrayList<MenuItem> menuItems = OrderItemtoMenuItems(orderItems);
+		ArrayList<MenuItem> discountItems = new ArrayList<>();
+		for (MenuItem mi : menuItems){
+			Collection<FoodCategory> fc = mi.getFoodCategories();
+			for (FoodCategory foodCat : fc){
+				if (foodCat.getTag().equals("pizza")){
+					discountItems.add(mi);
+				}
+			}
+		}
+		int normalPrice = 0;
+		for (MenuItem mi: menuItems){
+			normalPrice = normalPrice + mi.getPrice();
+		}
+		discountItems = BubbleSort(discountItems);
+		
+		return (normalPrice - getDiscPrice(discountItems));
+
+    }
+	
+	public ArrayList<MenuItem> OrderItemtoMenuItems(ArrayList<OrderItem> itemList){
+        ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
+        for(OrderItem item : itemList){
+            for(int i=0;i<item.getQuantity();i++){
+                menuItems.add(item.getMenuItem());
+            }
+        }
+        return menuItems;
+	}
+    
+	
+    public int getDiscPrice(ArrayList<MenuItem> menuItems){
+    	int discountPrice = 0;
+    	
         int itemsToPay;
-        
         itemsToPay = (menuItems.size()/2);
-        
         for (int i = 0; i < itemsToPay; i++){
         	MenuItem tmp = menuItems.get(i);
         	discountPrice += tmp.getPrice();
         }
-        
-//        if(menuItems.size()%2 == 1){
-//            itemsToPay = (menuItems.size()-1)/2;
-//            for(int i=0; i < itemsToPay; i++){
-//                MenuItem tmp = menuItems.get(i);
-//                discountPrice += tmp.getPrice();
-//            }
-//        }
-//        else{
-//            itemsToPay = (menuItems.size()-1)/2;
-//            for(int i=0; i < itemsToPay; i++){
-//                MenuItem tmp = menuItems.get(i);
-//                discountPrice += tmp.getPrice();
-//            }
-//        }
         return discountPrice;
     }
+    
 }
--- src/main/java/edu/avans/hartigehap/domain/DiscountTwoPlusOne.java
+++ src/main/java/edu/avans/hartigehap/domain/DiscountTwoPlusOne.java
@@ -1,21 +1,76 @@
 package edu.avans.hartigehap.domain;
 import java.util.ArrayList;
+import java.util.Collection;
 /**
  * Created by Student on 21-03-16.
  */
 public class DiscountTwoPlusOne extends DiscountStrategy {
-    public int CalculateDiscount(Bill bill){
-        int priceToPay = 0;
-        ArrayList<MenuItem> menuItems = this.getMenuItemList(bill);
-        int numberOfPizzas = menuItems.size();
-        int index = 0;
-        while(numberOfPizzas>=3){
-            menuItems.remove(menuItems.size()-1);
-            numberOfPizzas-=3;
-        }
-        for(MenuItem mi : menuItems){
-            priceToPay+=mi.getPrice();
-        }
-        return priceToPay;
+	
+	public int CalculateDiscount(Bill bill){
+		ArrayList<OrderItem> orderItems = new ArrayList();
+		for (Order order : bill.getAllOrders()){
+			orderItems.addAll(order.getOrderItems());
+		}
+		ArrayList<MenuItem> menuItems = OrderItemtoMenuItems(orderItems);
+		ArrayList<MenuItem> discountItems = new ArrayList<>();
+		for (MenuItem mi : menuItems){
+			Collection<FoodCategory> fc = mi.getFoodCategories();
+			for (FoodCategory foodCat : fc){
+				if (foodCat.getTag().equals("pizza")){
+					discountItems.add(mi);
+				}
+			}
+		}
+		int normalPrice = 0;
+		for (MenuItem mi: menuItems){
+			normalPrice = normalPrice + mi.getPrice();
+		}
+		discountItems = BubbleSort(discountItems);
+		
+		return (normalPrice - getDiscPrice(discountItems));
+
     }
+	
+	public ArrayList<MenuItem> OrderItemtoMenuItems(ArrayList<OrderItem> itemList){
+        ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
+        for(OrderItem item : itemList){
+            for(int i=0;i<item.getQuantity();i++){
+                menuItems.add(item.getMenuItem());
+            }
+        }
+        return menuItems;
+	}
+    
+	
+    public int getDiscPrice(ArrayList<MenuItem> menuItems){
+		int priceToPay = 0;
+		int numberOfPizzas = menuItems.size();
+		int index = 0;
+		while(numberOfPizzas>=3){
+		    menuItems.remove(menuItems.size()-1);
+		    numberOfPizzas-=3;
+		}
+		for(MenuItem mi : menuItems){
+		    priceToPay+=mi.getPrice();
+		}
+		return priceToPay;
+	}
+	
+	
+//	
+//    public int CalculateDiscount(Bill bill){
+//        int priceToPay = 0;
+//        ArrayList<MenuItem> menuItems = this.getMenuItemList(bill);
+//        int numberOfPizzas = menuItems.size();
+//        int index = 0;
+//        while(numberOfPizzas>=3){
+//            menuItems.remove(menuItems.size()-1);
+//            numberOfPizzas-=3;
+//        }
+//        for(MenuItem mi : menuItems){
+//            priceToPay+=mi.getPrice();
+//        }
+//        return priceToPay;
+//    }
+    
 }
